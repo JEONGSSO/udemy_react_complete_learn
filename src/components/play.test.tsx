70f378e51a8d27a95701239eb54246d69d2cfd74
jeongsso <rpsha33@naver.com>
2021-09-29 17:53:29 +0900

    mocked learn

--- src/components/play.test.tsx
+++ src/components/play.test.tsx
@@ -25,6 +25,7 @@ describe('testttt', () => {
 
   it('input test', async () => {
     const onChangeEvent = jest.fn();
+
     render(
       <div>
         <label htmlFor="asd">
@@ -45,5 +46,47 @@ describe('testttt', () => {
     expect(inputElem.value).toBe('아무거나입력');
 
     expect(onChangeEvent).toHaveBeenCalledTimes(6);
+  });
+
+  test.only('mock Test', () => {
+    const mockFn = jest.fn();
+    // mockReturnValue 가짜 함수의 리턴값을 지정
+    // mockFn.mockReturnValue('good');
+    // console.log('mockFn', mockFn());
+
+    // mockResolvedValue promise 객체로 리턴값을 받을 수 있다.
+    // mockFn.mockResolvedValue('아니');
+    // mockFn().then((res: string) => {
+    //   console.log(res);
+    // });
+
+    // mockImplementation
+    // mockFn.mockImplementation((name) => `good ${name}`);
+    // console.log(mockFn('Kim'));
+
+    // toBeCalledWith
+    // toHaveBeenCalledWith 어떤 인자와 같이 들어왔었는지 체크 가능
+
+    // mockFn('good');
+    // mockFn('bad');
+
+    // expect(mockFn).toBeCalledWith('bad');
+    // expect(mockFn).toHaveBeenCalledWith('good');
+
+    // spyOn 특정 함수의 리턴값과 인자를 확인 할 수 있다.
+    // const calculator = {
+    //   add: (a: number, b: number) => a + b,
+    // };
+    // const spyFn = jest.spyOn(calculator, 'add');
+    // const add = calculator.add(2, 3);
+
+    // expect(spyFn).toBeCalledWith(2, 3);
+    // expect(add).toBe(-1);
+
+    // spyOn mockImplementation 안의 동작까지도 재구성 가능
+    // spyFn.mockImplementation((a: number, b: number) => a - b);
+    // const sub = calculator.add(2, 3);
+    // expect(spyFn).toBeCalledWith(2, 3);
+    // expect(sub).toBe(-1);
   });
 });
