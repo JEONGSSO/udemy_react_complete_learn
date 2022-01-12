da2bae0d74936d89455b8d49dc0de070ee2e8a5b
jeongsso <rpsha33@naver.com>
2022-01-12 18:49:48 +0900

    router fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import { Login } from '@/components/login';
+import { Counter } from '@/components/counter';
+import { Monsters } from '@/components/monsters';
+import Test from '@/components/table/Test';
+import {
+  Shop,
+  CollectionPreview,
+  SignPage,
+} from '@/components/shop';
 import { Header } from './layout';
-
-import routes from '@/routes';
 
 import './App.scss';
 
@@ -12,9 +19,11 @@ export default () => (
     <Header />
     <main>
       <Routes>
-        {routes.map((route) => (
-          <Route path={route.path} {...route} element={<route.element />} />
-        ))}
+        <Route path="/" element={<div>123</div>}></Route>
+        <Route path="/login" element={Login}></Route>
+        <Route path="/counter" element={Counter}></Route>
+        <Route path="/table" element={Test}></Route>
+        <Route path="/monsters" element={Monsters}></Route>
       </Routes>
     </main>
   </>
