b4a6f856f961d8883fe81125fe60063053345a8d
jeongsso <rpsha33@naver.com>
2022-01-17 22:30:31 +0900

    route fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,75 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 
+import { Header } from './layout';
+import routes from './routes';
 import { Login } from '@/components/login';
 import { Counter } from '@/components/counter';
 import { Monsters } from '@/components/monsters';
 import Test from '@/components/table/Test';
 import { Shop, CollectionPreview, SignPage } from '@/components/shop';
-import { Header } from './layout';
 
 import './App.scss';
 
 export default () => {
+  const Main = useRoutes([
+    {
+      path: '/login',
+      element: <Login />,
+    },
+    {
+      path: '/counter',
+      element: <Counter />,
+    },
+    {
+      path: '/table',
+      element: <Test />,
+    },
+    {
+      path: '/monsters',
+      element: <Monsters />,
+    },
+    {
+      path: '/shop',
+      element: <Shop />,
+      children: [
+        {
+          path: '/shop/hats',
+          element: <Login />,
+        },
+        {
+          path: '/shop/jackets',
+          element: <Login />,
+        },
+        {
+          path: '/shop/sneakers',
+          element: <Login />,
+        },
+        {
+          path: '/shop/womens',
+          element: <Login />,
+        },
+        {
+          path: '/shop/mens',
+          element: <Login />,
+        },
+        {
+          path: '/shop/collection',
+          element: <CollectionPreview />,
+        },
+        {
+          path: '/shop/signin',
+          element: <SignPage />,
+        },
+      ],
+    },
+  ]);
+
   return (
     <>
       <Header />
-      <main>
-        <Routes>
-          <Route path="shop" element={<Shop />}>
-            <Route path="hats" element={<Login />} />
-            <Route path="jackets" element={<Login />} />
-            <Route path="sneakers" element={<Login />} />
-            <Route path="womens" element={<Login />} />
-            <Route path="mens" element={<Login />} />
-            <Route path="signin" element={<SignPage />} />
-            <Route path="collection" element={<CollectionPreview />} />
-          </Route>
-          <Route path="login" element={<Login />} />
-          <Route path="counter" element={<Counter />} />
-          <Route path="table" element={<Test />} />
-          <Route path="monsters" element={<Monsters />} />
-        </Routes>
-      </main>
+      <main>{Main}</main>
+      <footer>Footer</footer>
     </>
   );
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,6 @@
-import { RouteProps } from 'react-router-dom';
+import { RouteObject } from 'react-router-dom';
 
-import { Login } from '@/components/login';
-import { Counter } from '@/components/counter';
-import { Monsters } from '@/components/monsters';
-import Test from '@/components/table/Test';
-import { Shop, CollectionPreview, SignPage } from '@/components/shop';
-
-type RoutesProps = RouteProps & {
+type RoutesProps = RouteObject & {
   name: string;
 };
 
@@ -14,66 +8,23 @@ const routes: RoutesProps[] = [
   {
     name: 'login',
     path: '/login',
-    element: Login,
   },
   {
     name: 'counter',
-    path: '/counter/:id?',
-    element: Counter,
+    path: '/counter',
   },
   {
     name: 'table',
     path: '/table',
-    element: Test,
   },
   {
     name: 'monsters',
     path: '/monsters',
-    element: Monsters,
   },
   {
     name: 'shop',
     path: '/shop',
-    element: Shop,
   },
-  //   routes: [
-  //     {
-  //       name: 'hats',
-  //       path: '/shop/hats',
-  //       element: Login,
-  //     },
-  //     {
-  //       name: 'jackets',
-  //       path: '/shop/jackets',
-  //       element: Login,
-  //     },
-  //     {
-  //       name: 'sneakers',
-  //       path: '/shop/sneakers',
-  //       element: Login,
-  //     },
-  //     {
-  //       name: 'womens',
-  //       path: '/shop/womens',
-  //       element: Login,
-  //     },
-  //     {
-  //       name: 'mens',
-  //       path: '/shop/mens',
-  //       element: Login,
-  //     },
-  //     {
-  //       name: 'collection',
-  //       path: '/shop/collection',
-  //       element: CollectionPreview,
-  //     },
-  //     {
-  //       name: 'signin',
-  //       path: '/shop/signin',
-  //       element: SignPage,
-  //     },
-  //   ],
-  // },
 ];
 
 export default routes;
